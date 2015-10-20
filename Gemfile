c825ae749785a5fd3d621fd9a57aa0d50f3fb0aa
Daniel Klopp <dklopp@infoblox.com>
2015-10-19 20:02:27 -0700

    Updating configuration

--- Gemfile
+++ Gemfile
@@ -3,8 +3,8 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in github_watchdog.gemspec
 # I know I should do them in the gemspec, but darnit, I hate gemspec and I love
 # Gemfile
-gem 'slack-ruby-bot'
-gem 'octokit'
-gem 'daemons'
+#gem 'slack-ruby-bot'
+#gem 'octokit'
+#gem 'daemons'
 
 gemspec
--- lib/github_watchdog.rb
+++ lib/github_watchdog.rb
@@ -1,9 +1,21 @@
-require "github_watchdog/version"
+#require "github_watchdog/version"
 
 #module GithubWatchdog
   # Your code goes here...
 #end
 
-
-#require 'daemons'
-
+# There is a cooler way to do this with: http://daemons.rubyforge.org/
+# But this works for the trivial use case and the time constraints
+options={}
+File.open("/etc/github_watchdog.conf") do |f|
+	f.each_line do |line|
+		elements=line.split('=')
+		options={elements[0] => elements[1]}
+	end
+end
+loop do 
+	options.each do |key, value|
+		puts "#{key} - #{value}"
+	end
+	sleep(30)
+end
