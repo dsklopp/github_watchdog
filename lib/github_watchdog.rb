f1a190e4ea79ae10d1d30a5daee525aefeef5045
Daniel Klopp <dklopp@infoblox.com>
2015-10-19 19:52:39 -0700

    Initial commit

---
+++ lib/github_watchdog.rb
@@ -0,0 +1,9 @@
+require "github_watchdog/version"
+
+#module GithubWatchdog
+  # Your code goes here...
+#end
+
+
+#require 'daemons'
+
---
+++ lib/github_watchdog/version.rb
@@ -0,0 +1,3 @@
+module GithubWatchdog
+  VERSION = "0.1.0"
+end
